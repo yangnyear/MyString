c4b24b232a55bb49f860ce12b825a836467b5660
羊荣毅_L <_em@163.com>
2016-10-20 20:53:05 +0800

    Initial commit

---
+++ src/com/company/MyString.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.net.StandardSocketOptions;
+
+public class MyString {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte[]b={10,20,30,40,50,60};
+        System.out.println(new String(b));
+        char[]a={'a','c','d'};
+        System.out.println(a);
+        System.out.println(new StringBuffer("dsgafafds"));
+
+    }
+}
